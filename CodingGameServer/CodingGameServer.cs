5057d0b168ff457ba814d603c1fe7806e8ac96ea
KYapp <kukiyomenaisapp@gmail.com>
2022-07-28 17:40:30 +0900

    Init

---
+++ CodingGameServer/CodingGameServer.cs
@@ -0,0 +1,82 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingGameBase
+{
+    public class CodingGameServer
+    {
+        public int port { get; private set; }
+        public Dictionary<string, byte[]> sharedata;
+        public byte[] data;
+        public Action Tick;
+
+        private HttpListener listener;
+
+        public CodingGameServer()
+        {
+            this.port = 59108;
+            sharedata = new Dictionary<string, byte[]>();
+        }
+
+        public CodingGameServer(int port)
+        {
+            this.port = port;
+            sharedata = new Dictionary<string, byte[]>();
+        }
+
+        public void StartServer()
+        {
+            listener = new HttpListener();
+
+            listener.Prefixes.Clear();
+            listener.Prefixes.Add("http://localhost:" + port + "/");
+            Task.Run(() => OnProcess());
+        }
+
+        public void OnProcess()
+        {
+            try
+            {
+                listener.Start();
+
+                while (true)
+                {
+                    HttpListenerContext context = listener.GetContext();
+                    HttpListenerRequest request = context.Request;
+
+                    HttpListenerResponse response = context.Response;
+
+                    if (request != null)
+                    {
+                        byte[] text = Encoding.UTF8.GetBytes("hogehogefoo");
+                        data = GetByteArrayFromStream(request.InputStream);
+                        
+                        if (Tick != null) Tick();
+
+                        response.OutputStream.Write(text, 0, text.Length);
+                    }
+                    else
+                    {
+                        response.StatusCode = 404;
+                    }
+                    response.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        private static byte[] GetByteArrayFromStream(Stream sm)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                sm.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+using CodingGameBase;
+
+CodingGameServer codingGameServer = new CodingGameServer();
+
+codingGameServer.Tick = Tick;
+
+codingGameServer.StartServer();
+Console.ReadLine();
+
+void Tick()
+{
+    Console.WriteLine(Encoding.UTF8.GetString(codingGameServer.data));
+}
